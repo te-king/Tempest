425dcfe96e027ebd1bf606425070a33ddc7fa432
te-king <thomas.e.king@icloud.com>
2019-02-17 15:12:47 +1100

    Update Program.kt
    added info log property

--- src/main/kotlin/wrappers/opengl/Program.kt
+++ src/main/kotlin/wrappers/opengl/Program.kt
@@ -1,8 +1,11 @@
 package wrappers.opengl
 
-import org.lwjgl.opengl.GL45C.*
+import org.lwjgl.opengl.GL45C.glDeleteProgram
+import org.lwjgl.opengl.GL45C.glGetProgramInfoLog
 
 class Program(device: Device, val id: Int): Device.DeviceResource(device) {
+
+    val infoLog get() = glGetProgramInfoLog(id)
 
     override fun delete() = glDeleteProgram(id)
 

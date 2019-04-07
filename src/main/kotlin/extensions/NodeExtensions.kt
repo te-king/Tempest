b7b1bee44dd4719ae9b84f491b37ec24489b28d9
te-king <thomas.e.king@icloud.com>
2019-04-07 20:24:47 +1000

    fixed find referencing private property

--- src/main/kotlin/extensions/NodeExtensions.kt
+++ src/main/kotlin/extensions/NodeExtensions.kt
@@ -8,8 +8,8 @@ import kotlin.reflect.KClass
 infix fun<T: Component> Node.find(type: KClass<T>): T? {
     val transform = this get Transform::class ?: return null
 
-    for (child in transform.childeren) return child.node get type ?: continue
-    for (child in transform.childeren) return child.node find type ?: continue
+    for (child in transform) return child.node get type ?: continue
+    for (child in transform) return child.node find type ?: continue
 
     return null
 }

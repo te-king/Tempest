d146943d0abc36d032556e7c74a781b55b3f7ca7
te-king <thomas.e.king@icloud.com>
2019-04-22 18:32:24 +1000

    Added size extensions to double type

--- src/main/kotlin/math/Extensions.kt
+++ src/main/kotlin/math/Extensions.kt
@@ -2,3 +2,6 @@ package math
 
 inline val Float.Companion.SIZE_BITS get() = Int.SIZE_BITS
 inline val Float.Companion.SIZE_BYTES get() = Int.SIZE_BYTES
+
+inline val Double.Companion.SIZE_BITS get() = Long.SIZE_BITS
+inline val Double.Companion.SIZE_BYTES get() = Long.SIZE_BYTES

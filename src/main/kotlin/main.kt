403cc4b835486bd0396d9828418eb3b2bf334cab
te-king <thomas.e.king@icloud.com>
2019-04-24 01:15:06 +1000

    Replaced debug rotation script with RigidBody

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,6 +4,7 @@ import engine.runtime.Client
 import engine.world.*
 import extensions.find
 import math.Float3
+import math.Quaternion
 
 fun main() {
 
@@ -12,16 +13,8 @@ fun main() {
     // Load object
     run {
         val node = resourceAt("""assets/bunny.obj""").loadNode(scene)!!
-        //val nodeRotator = node add RotationScript::class
-        val nodeRenderer = node find MeshRenderer::class
-
-        nodeRenderer?.let {
-            it.pairs[0] = it.pairs[0].first to Client.raymarchShader.Material()
-        }
-
-        val nodeTransform = node add Transform::class
-        nodeTransform.scale = Float3(100f, 100f, 100f)
-
+        val nodePhysics = node add RigidBody::class
+        nodePhysics.rotationDelta = Quaternion.fromAxisAngle(Float3.up, 1f)
     }
 
     // Initialize camera
@@ -30,7 +23,7 @@ fun main() {
     val cameraController = cameraNode add DebugCameraController::class
     val cameraTransform = cameraNode add Transform::class
     cameraCamera.window = Client.window
-    cameraTransform.translation = Float3(0f, 0f, 10f)
+    cameraTransform.translation = Float3(0f, 0f, 5f)
 
     val lamp = Node(scene)
     val lampLightEmitter = lamp add LightEmitter::class

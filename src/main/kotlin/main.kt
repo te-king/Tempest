6f4e9ba2d65bc818121a729ee7c49130b2f97179
te-king <thomas.e.king@icloud.com>
2019-04-24 20:38:46 +1000

    Changes to debugging entry point

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,32 +1,37 @@
-import engine.graphics.RaymarchShader
 import engine.graphics.resourceAt
 import engine.runtime.Client
 import engine.world.*
-import extensions.find
+import extensions.findAll
 import math.Float3
 import math.Quaternion
+import kotlin.system.measureNanoTime
 
 fun main() {
 
     val scene = Client.scene
 
+    val worldNode = Node(scene, "World")
+    val worldController = worldNode add WorldController::class
+    worldController.gravity = Float3.zero
+
     // Load object
     run {
         val node = resourceAt("""assets/bunny.obj""").loadNode(scene)!!
-        val nodePhysics = node add RigidBody::class
-        nodePhysics.rotationDelta = Quaternion.fromAxisAngle(Float3.up, 1f)
+        val nodeTransform = node add Transform::class
+        val nodePhysics = node add PhysicsBody::class
+
+        nodeTransform.parent = worldNode add Transform::class
+        nodePhysics.rotationDelta = Quaternion.fromAxisAngle(Float3.up, 1f) * Quaternion.fromAxisAngle(Float3.right, 0.912f)
+        nodePhysics.translationDelta = Float3(0f, 1f, 0f)
     }
 
     // Initialize camera
     val cameraNode = Node(scene)
-    val cameraCamera = cameraNode add Camera::class
-    val cameraController = cameraNode add DebugCameraController::class
     val cameraTransform = cameraNode add Transform::class
+    val cameraController = cameraNode add DebugCameraController::class
+    val cameraCamera = cameraNode add Camera::class
     cameraCamera.window = Client.window
     cameraTransform.translation = Float3(0f, 0f, 5f)
-
-    val lamp = Node(scene)
-    val lampLightEmitter = lamp add LightEmitter::class
 
     // Run the client program
     Client.run()
